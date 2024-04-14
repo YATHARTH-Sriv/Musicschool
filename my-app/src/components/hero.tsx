7e999dd5983d5112d2b32c0a5c3d9b8bba93d564
yatharth srivastava <yatharthsriv18@gmail.com>
2024-04-14 09:39:27 +0530

    adding key values to props

--- my-app/src/components/hero.tsx
+++ my-app/src/components/hero.tsx
@@ -17,14 +17,14 @@ function HeroSection() {
             >Learn The Art Of Music</h1>
             <p
             className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
-            >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+            >Dive into our comprehensive music courses and transform your musical journey today. Whether you&apos;re a beginner or looking to refine your skills, join us to unlock your true potential.</p>
             <div className="mt-4">
                 <Link href={"/courses"}>
                     <Button
                     borderRadius="1.75rem"
                     className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
                     >
-                    Explore courses
+                    Explore
                     </Button>
                 </Link>
             </div>
